8ae6c9bd81499cd6b5d001502c54d4c74d41d5ce
Shajid <shajid1biz@gmail.com>
2024-07-02 01:31:02 +0530

    completed the first challenge

---
+++ Hello-World-Function/HelloWorldFunction.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Function}
+ */
+
+
+const createHelloWorld = () => {
+  return function (...args) {
+      return "Hello World"
+  }
+}
+
+const f = createHelloWorld()
+f()
+
+/**
+* const f = createHelloWorld();
+* f(); // "Hello World"
+*/

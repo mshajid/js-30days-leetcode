cb2748e6a546bbd13a39293522491c9966641bd6
Shajid <shajid1biz@gmail.com>
2024-07-19 21:29:00 +0530

    Improvised the code block for efficiency

--- Day-3-Counter-II/Counter2.js
+++ Day-3-Counter-II/Counter2.js
@@ -5,23 +5,35 @@
 var createCounter = function (init) {
   let countInit = init;
 
-  function increment() {
-    return ++countInit;
-  }
+  //   function increment() {
+  //     return ++countInit;
+  //   }
 
-  function decrement() {
-    return --countInit;
-  }
+  //* Efficient Way to Increment in 1 Line
+  const increment = () => ++countInit;
+
+  //   function decrement() {
+  //     return --countInit;
+  //   }
+
+  const decrement = () => --decrement;
 
   function reset() {
     countInit = init;
     return countInit;
   }
 
+  //   return {
+  //     increment: increment,
+  //     decrement: decrement,
+  //     reset: reset,
+  //   };
+
+  //* If the object and key are same name
   return {
-    increment: increment,
-    decrement: decrement,
-    reset: reset,
+    increment,
+    decrement,
+    reset,
   };
 };
 

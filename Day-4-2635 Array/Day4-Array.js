6a6324fbeeb83246ddba616d01d2c5c1919f49e3
Shajid <shajid1biz@gmail.com>
2024-07-21 10:06:16 +0530

    Added new example

--- Day-4-2635 Array/Day4-Array.js
+++ Day-4-2635 Array/Day4-Array.js
@@ -4,11 +4,18 @@
  * @return {number[]}
  */
 var map = function (arr, fn) {
-    const res = new Array(arr.length);
+  const res = new Array(arr.length);
 
-    for (const i in arr) {
-        res[i] = fn(arr[i], Number(i))
-    }
+  for (const i in arr) {
+    res[i] = fn(arr[i], Number(i));
+  }
 
-    return res;
+  return res;
 };
+
+// Shajid's Example
+const arr = [1, 2, 3, 4];
+const fn = (element, index) => element * index;
+
+const result = map(arr, fn);
+console.log(result);
